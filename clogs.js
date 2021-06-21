28b16a654ab144e80391d037900c5d9262554c73
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-06-21 10:27:19 -0500

    Add files via upload

---
+++ clogs.js
@@ -0,0 +1,66 @@
+{
+	"packages": [
+		{	
+			"version": "11",
+			"package": "com.whatsapp",
+			"status": "mod_tips",
+			"title": "XWhatsApp",
+			"note": "false",
+			"msgtxt": "http://webs.redirectme.net/mod/prueba.html",
+			"download": "https://github.com/WhatsaMods/TheFiles/raw/master/x/x2.21.11.11.apk",
+			"update": [
+				{
+					"languaje": "es",
+					"theme": "dark",
+					"changelog": "http://webs.redirectme.net/mod/mod-es-d.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "dark",
+					"changelog": "http://webs.redirectme.net/mod/mod-en-d.html"
+				},
+				{
+					"languaje": "es",
+					"theme": "light",
+					"changelog": "http://webs.redirectme.net/mod/mod-es-l.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "light",
+					"changelog": "http://webs.redirectme.net/mod/mod-en-l.html"
+				}
+			]
+		},
+		{
+			"version": "2.21.12.7",
+			"package": "com.whatsapp.w4b",
+			"status": "update_title",
+			"title": "WhatsApp",
+			"note": "false",
+			"msgtxt": "http://webs.redirectme.net/mod/prueba.html",
+			"download": "https://github.com/WhatsaMods/TheFiles/raw/master/x/w11.9.apk",
+			"update": [
+				{
+					"languaje": "es",
+					"theme": "dark",
+					"changelog": "http://webs.redirectme.net/mod/mod-es-d.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "dark",
+					"changelog": "http://webs.redirectme.net/mod/mod-en-d.html"
+				},
+				{
+					"languaje": "es",
+					"theme": "light",
+					"changelog": "http://webs.redirectme.net/mod/mod-es-l.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "light",
+					"changelog": "http://webs.redirectme.net/mod/mod-en-l.html"
+				}
+			]
+		}
+	]
+}
