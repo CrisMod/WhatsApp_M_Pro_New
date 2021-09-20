4a057a53d52538cda2486c2c88a68bb1d3f21ea5
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-09-19 19:14:13 -0500

    Update clogs.js

--- clogs.js
+++ clogs.js
@@ -83,7 +83,7 @@
 			]
 		},
                 {	
-			"version": "23.00",
+			"version": "23.01",
 			"package": "com.mwhatsapp",
 			"status": "update_title",
 			"title": "WhatsApp {M}",
@@ -94,32 +94,32 @@
 				{
 					"languaje": "es",
 					"theme": "dark",
-					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M/dark/changelog-en.html"
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/dark/changelog-en.html"
 				},
 				{
 					"languaje": "en",
 					"theme": "dark",
-					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M/dark/changelog-en.html"
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/dark/changelog-en.html"
 				},
 				{
 					"languaje": "pt-br",
 					"theme": "dark",
-					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M/dark/changelog-pt.html"
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/dark/changelog-pt.html"
 				},
 				{
 					"languaje": "es",
 					"theme": "light",
-					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M/light/changelog-en.html"
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/light/changelog-en.html"
 				},
 				{
 					"languaje": "pt-br",
 					"theme": "light",
-					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M/light/changelog-pt.html"
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/light/changelog-pt.html"
 				},
 				{
 					"languaje": "en",
 					"theme": "light",
-					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M/light/changelog-en.html"
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/light/changelog-en.html"
 				}
 			]
 		}
