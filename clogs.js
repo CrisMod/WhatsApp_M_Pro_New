521bcb427dd500c3ad76e69d6933e1f558cc63e0
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-10-26 21:10:38 -0500

    Update clogs.js

--- clogs.js
+++ clogs.js
@@ -1,18 +1,18 @@
 {
 	"packages": [
 		{	
-			"version": "17.00",
+			"version": "17.10",
 			"package": "com.whatsapp",
 			"status": "update_title",
-			"title": "WhatsApp {M Pro}",
+			"title": "WhatsApp {M Pro} 17.10",
 			"note": "false",
 			"msgtxt": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-en.html",
-			"download": "http://fumacrom.com/1e3Mr",
+			"download": "http://fumacrom.com/2g9hu",
 			"update": [
 				{
 					"languaje": "es",
 					"theme": "dark",
-					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/dark/changelog-en.html"
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/dark/changelog-es.html"
 				},
 				{
 					"languaje": "en",
@@ -27,7 +27,7 @@
 				{
 					"languaje": "es",
 					"theme": "light",
-					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/light/changelog-en.html"
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/light/changelog-es.html"
 				},
 				{
 					"languaje": "pt-br",
