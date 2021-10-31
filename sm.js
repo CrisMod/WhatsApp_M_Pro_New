da3f38fbf9645d7396e69a05a6d072f883660033
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-10-30 22:47:28 -0500

    Add files via upload

---
+++ sm.js
@@ -0,0 +1,35 @@
+{
+	"packages": [
+		{	
+			"version": "2.21.22.22",
+			"package": "com.whatsapp",
+			"status": "update_title",
+			"title": "WhatsApp {Super Mini}",
+			"note": "false",
+			"msgtxt": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-en.html",
+			"download": "http://fumacrom.com/1e3Mr",
+			"update": [
+				{
+					"languaje": "es",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/dark/changelog-en.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/dark/changelog-en.html"
+				},
+				{
+					"languaje": "es",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/light/changelog-en.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp_M_Pro_New/main/M%20Pro/light/changelog-en.html"
+				}
+			]
+		}
+	]
+}
